3ee9f440f253d90cbbe16261e1cf4f965e9e8083
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-05-25 21:02:47 +0530

    Variables and datatypes in js

---
+++ lecture 40/index..js
@@ -0,0 +1,59 @@
+// ~ Variables
+// var age = 25;
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+    // console.log(age); 
+// }
+// give error
+// console.log(age);
+// solve();
+
+// var x = 30;
+// var x = 40;
+
+
+// {
+// let a = 10;
+// }
+// console.log(a);
+
+// let a = 20;
+// a = 30;
+// console.log(a);
+
+
+// let a = 10;
+// a = "pritam";
+// a = true ;
+// a = null;
+// console.log(a);
+
+
+// const a  = 28;
+// console.log(a);
+
+// ~ Data Types
+//dynamically typed language
+// let marks = 20.23345;   //numbers
+// marks = "pritam" // strings
+// marks = true; // boolean
+// console.log(marks);
+
+
+// let marks;  //undefined
+// console.log(marks);
+
+// let marks = null;  // null
+// console.log(marks);
+
+// let marks = 654145648451564564569876512156456121565465625646554565498615445648454545454854519692556484964548765498456184985448795454787848946312549845; //  bigint
+// console.log(marks);
+
+
+// marks = false; 
+// console.log(typeof(marks));  //  find data types...
+
