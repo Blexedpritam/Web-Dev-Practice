2dc967006bc2a1b22c1b26ddb85e0810788d11b4
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-05-26 19:53:52 +0530

    Add index.html and index.js: Create initial HTML structure and implement Arithmetic , Assignment  ,Comparison , Logical ,Ternary  and bitwise operations

---
+++ lecture 41/index.js
@@ -0,0 +1,60 @@
+//  arithmetic operator
+
+// let a = 2;
+// let b = 5;
+
+// console.log(a**b);
+
+// Assignment operator
+
+// let a = 5;
+// let b = 10;
+
+// a = a + 10;
+// a += 10;
+// console.log(a);
+
+// a = a-2;
+// a -= 2;
+// console.log(a);
+
+// a = a/2;
+// a /= 2;
+// console.log(a);
+
+// a = a*3;
+// a *= 3;
+// console.log(a);
+
+
+// comparison operator
+
+// let a = 10;
+// let b = 5;
+
+// console.log(50 >= 5);
+// console.log('5' == 5);  // lose equality
+// console.log('5' === 5); //strict equality
+
+//Ternary operator
+// let age = 36;
+// let status1 =(age > 18) ? 'i can vote' : 'i cannot vote';
+
+// console.log(status1);
+
+// let ans = (true && true && true);
+// let ans = (false|| false || true ) ;
+// let ans = !(true);
+// console.log(ans);
+
+//  Non - Boolean 
+// console.log( false || 'pritam');
+// console.log(false || 3 || 4 || 5 ); // Short circuting
+
+// Bitwise Operator
+// console.log(2 & 5);
+// console.log( 2 | 5);
+// console.log(~(0));
+// console.log( 2 ^ 2);
+console.log(10 >> 1);  // right shift
+console.log(10 << 5); // left shift
