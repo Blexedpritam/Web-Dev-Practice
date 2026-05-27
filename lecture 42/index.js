2a28fbac50adfdda98fbc671ba144f228b72fab8
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-05-27 22:26:27 +0530

    Refactor index.js: Comment out unused code snippets for clarity

---
+++ lecture 42/index.js
@@ -0,0 +1,76 @@
+// for(let a=1; a<=10; a++){
+//     console.log('pritam');
+// }
+// let i = 1;
+// while(i<=10){
+//     console.log('pritam');
+//     i++;
+// }
+
+// for (let i=1; i<=5; i++){
+//       console.log(i);
+// }
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// for(let i=5; i>0; i--){
+//     console.log(i);
+// }
+// let i =5;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// for(let i=1; i<=6;i++){
+//     if(i ==4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// while(i<=6){
+//     if(i ==3){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// for(let i=1;  i<=5; i++){
+//     if(i == 3){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+let i = 1;
+while(i<=5){
+    if(i ==3){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+    i++; // if i forget to updations.. it become infinite loop..!
+}
+
+// for (;;) {
+//      console.log("Hello");  //foor infinite 
+// }
+
+// while (true) {
+//     console.log("Hi");
+// }       // for infinite short cut ..!
+
