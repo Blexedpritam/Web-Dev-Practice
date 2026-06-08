809158d8a90653caf78bc94a1d1ac276c9f014e5
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-06-08 18:23:15 +0530

    Add index.js: Implement block and function scope examples with console logs

---
+++ lecture 46/index.js
@@ -0,0 +1,64 @@
+//  BLOCK SCOPE 
+// console.log(age);
+// {
+//     var age =25;
+// }
+// console.log(age);
+
+// {
+//     let age = 25;
+// }
+// console.log(age);
+
+{
+    const age =19;
+}
+
+console.log(age);
+
+
+// FUNCTION SCOPE
+
+// function sayHello(){
+//     let name = 'Earth';
+//     console.log("hello dunia" ,name);
+// }
+
+// // console.log(name);
+// sayHello();
+
+
+
+// //Global Scope 
+
+
+// let age = 15;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true){
+//     console.log(age);
+// }
+
+// for(let i =0;i<2;i++){
+//     console.log(age);
+// }
+
+// function sayHello (){
+//     console.log("Hi :" ,age);
+// }
+
+// sayHello();
+
+
+
+
+
+
+
+
+
