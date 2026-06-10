971a951fac8cb03a992a431b820af89d423202a6
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-06-10 23:03:53 +0530

    Refactor index.js: Comment out console log statements for clarity

--- lecture 48/index.js
+++ lecture 48/index.js
@@ -1,28 +1,41 @@
-console.log("pritam");
+// console.log("pritam");
 
-console.log(Math.PI);
+// console.log(Math.PI);
 
-console.log(Math.max(655,622,689,72,90));
+// console.log(Math.max(655,622,689,72,90));
 
-console.log(Math.min(70,80,90,12,28,45,11));
+// console.log(Math.min(70,80,90,12,28,45,11));
 
-console.log(Math.round(1.6));
+// console.log(Math.round(1.6));
 
-console.log(Math.floor(1.9));
+// console.log(Math.floor(1.9));
 
-console.log(Math.ceil(1.9));
+// console.log(Math.ceil(1.9));
 
-console.log(Math.abs(-5));
+// console.log(Math.abs(-5));
 
-console.log(Math.sqrt(5));
+// console.log(Math.sqrt(5));
 
-console.log(Math.pow(2,10));
+// console.log(Math.pow(2,10));
 
-console.log(Math.sin(30));
+// console.log(Math.sin(30));
 
-console.log(Math.cos(0));
+// console.log(Math.cos(0));
 
-console.log(Math.tan(-260));
+// console.log(Math.tan(-260));
 
 
+let curr = new Date();
+
+let date = new Date('october 02 2006 07:20');
+
+let newdate = new Date(2006,9,2, 7);
+
+// console.log(newdate);
+
+// console.log(newdate.getDay());
+
+console.log(newdate.setFullYear(2001));
+
+console.log(newdate);
 
