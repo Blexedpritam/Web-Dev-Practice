1dfdc71478260f9d0bf9787b62042337d6bb3af4
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-06-15 22:15:47 +0530

    ERROR HANDLING

--- lecture 50/index.js
+++ lecture 50/index.js
@@ -8,3 +8,39 @@
 
 //Reference error 
 // console.log(x);
+
+
+// try{
+//     console.log("Try block start here");
+//     // error -> reference error 
+//     console.log(x);
+//     console.log("Try block ends here");
+ 
+// }
+// catch(err){
+//     //define krte h,error k saath aap kya krna chate h
+//     //retry logic 
+//     //fallback machansim 
+//     // logging 
+//     //custom error
+//     console.log("I am inside the catch block");
+//     console.log("Your error is here :" ,err);
+// }
+// finally{
+//     console.log("i will run everytime , as i am finally block");
+// }
+
+
+// try{
+//     //Refernece error 
+//     console.log(x);
+// }
+// catch(err){
+//     throw new Error("Bhai phele declare kro , fhir print krna");
+
+// }
+
+let errorCode = 100;
+if (errorCode == 100){
+    throw new Error ('Invalid json');
+}
