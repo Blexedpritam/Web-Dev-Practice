def9b7225c65cfc2596e761e315834655b99f106
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-06-18 00:07:41 +0530

    Add index.html and index.js: Create initial HTML structure and implement child removal in JavaScript

---
+++ lecture 51/index.js
@@ -0,0 +1,10 @@
+// let mydiv = document.querySelector('#mydiv')
+
+// let newElement = document.createElement('span');
+// newElement.textContent ="pritam kumar"
+
+// mydiv.insertAdjacentElement("afterend" , newElement);
+
+let parent = document.querySelector("#mydiv");
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
