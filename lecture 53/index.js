762d7800cb40a5c30f3f1d95beb490c06fe94d5b
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-06-20 12:31:13 +0530

    Refactor index.html and index.js: Update HTML structure and enhance click event handling for paragraphs

--- lecture 53/index.js
+++ lecture 53/index.js
@@ -10,12 +10,29 @@
 
 // fpara.removeEventListener('click',changeText);
 
-function anchorAction(event){
-    event.preventDefault();
-    let anchorElement = document.getElementById('fanchor');
+// function anchorAction(event){
+//     event.preventDefault();
+//     let anchorElement = document.getElementById('fanchor');
     
-    anchorElement.textContent ="Click done Bade Bhaiya"
+//     anchorElement.textContent ="Click done Bade Bhaiya"
+// }
+
+// let anchorElement = document.getElementById('fanchor');
+// anchorElement.addEventListener('click' ,anchorAction);
+
+
+// let paras = document.querySelectorAll('p');
+
+function alertPara(){
+    if(event.target.nodeName =="SPAN"){
+    alert('You have click on para :' + event.target.textContent);
+    }
 }
 
-let anchorElement = document.getElementById('fanchor');
-anchorElement.addEventListener('click' ,anchorAction);
+// for (let i = 0; i <paras.length; i++){
+//     let para = paras[i];
+//     para.addEventListener('click' ,alertPara)
+// }
+
+let myDiv = document.getElementById('wrapper');
+myDiv.addEventListener('click' ,alertPara)
